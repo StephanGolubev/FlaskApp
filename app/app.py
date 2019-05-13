e322c646a1bd532b06e1fced39ec5e56e668e822
Stephan <golubevstephan@gmail.com>
2019-05-13 12:28:29 +0300

    Сервер готов

---
+++ app/app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/extention', methods=['POST'])
+def extention():
+    if request.method == 'POST':
+        import json
+        import network
+
+        # получаем запрос
+        response = request.json
+        # переводим в массив
+        response2 = json.dumps(response)
+        response2 = json.loads(response2)
+        my_list = []
+        my_dict = {}
+        my_string = ''
+
+
+        # лупим через массив скриптов
+        number = 0
+        for i in response2['code']:
+            x = network.classify(i)
+            if x == 1:
+                my_list.append(number);
+                my_string = my_string + str(number) + ','
+                number = number + 1
+            else:
+                number = number + 1
+        # for i in range(len(my_list)):
+        #     my_dict[i] = my_list[i]
+
+        # json_new = json.dumps(my_dict)
+        return repr(my_string)
+
+
+@app.route("/neyr")
+def nayr():
+    return "hello"
+
+if __name__ == '__main__':
+    app.run()
